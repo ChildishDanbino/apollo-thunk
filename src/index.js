606d54f3bf0330955edbbccc5d057891be8166aa
Daadler6 <dadler@madmobile.com>
2018-04-24 10:47:44 -0400

    adding gitignore and additional query options
    adding gitignore and additional query options

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 export default function apolloThunk(graphQlClient) {
-	return ({ dispatch, getState }) => {
+	return ({dispatch, getState}) => {
 		return next => (action) => {
 			if (typeof action === 'function') {
 				// thunk
 				return action(dispatch, getState);
 			}
 
-			const { mutation, query, types, variables, fetchPolicy = 'cache-first', ...rest } = action;
+			const {
+				mutation,
+				query,
+				types,
+				variables,
+				fetchPolicy = 'cache-first',
+				errorPolicy = 'none',
+				pollInterval,
+				notifyOnNetworkStatusChange = false,
+				context,
+				...rest
+			} = action;
 
 			if (!query && !mutation) {
 				//no query, no mutation, nothing to do here
@@ -15,21 +26,29 @@ export default function apolloThunk(graphQlClient) {
 
 			const [REQUEST, SUCCESS, FAILURE] = types;
 
-			next({ ...rest, type: REQUEST });
+			next({...rest, type: REQUEST});
 
 			// graphql mutations
 			if (mutation) {
-				return graphQlClient.mutate({ mutation, variables }).then(
-					result => next({ ...rest, ...result, type: SUCCESS }),
-				error => next({ ...rest, error, type: FAILURE })
-			);
+				return graphQlClient.mutate({mutation, variables}).then(
+					result => next({...rest, ...result, type: SUCCESS}),
+					error => next({...rest, error, type: FAILURE})
+				);
 			}
 
 			// graphql queries
-			return graphQlClient.query({ query, variables, fetchPolicy }).then(
-				result => next({ ...rest, ...result, type: SUCCESS }),
-			error => next({ ...rest, error, type: FAILURE })
-		);
+			return graphQlClient.query({
+				query,
+				variables,
+				fetchPolicy,
+				errorPolicy,
+				pollInterval,
+				notifyOnNetworkStatusChange,
+				context
+			}).then(
+				result => next({...rest, ...result, type: SUCCESS}),
+				error => next({...rest, error, type: FAILURE})
+			);
 		};
 	};
 }
